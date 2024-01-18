f8931c6b88e1f26a102613c3ce01388e162c4934
Александр <zpeoplle@yandex.ru>
2024-01-18 12:40:10 +0300

    сделал класс Node<Task> внутренним для класса InMemoryHistoryManager

--- src/org/tasktracker/manager/InMemoryHistoryManager.java
+++ src/org/tasktracker/manager/InMemoryHistoryManager.java
@@ -95,4 +95,17 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
         size--;
     }
+
+    public class Node <Task> {
+        public Node<Task> prev;
+        public Task data;
+        public Node<Task> next;
+
+
+        public Node(Node<Task> prev, Task data, Node<Task> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
 }
